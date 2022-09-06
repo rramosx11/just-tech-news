51f8e7fe7f94ae1afb97a1b22ae00331fa64f56c
rramosx11 <rodolforamosd11@gmail.com>
2022-09-05 22:08:04 -0400

    added edit and delete post features

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,6 +20,8 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({ helpers });
 
